d1ccfc33ceeb37ce2d3b995242efe27e05711da4
yushika99 <it20105952@my.sliit.lk>
2021-08-08 16:02:23 +0530

    2021/08/08 4.01 pm

--- app/src/main/java/com/example/rs_fy/MainActivity.java
+++ app/src/main/java/com/example/rs_fy/MainActivity.java
@@ -1,14 +1,34 @@
 package com.example.rs_fy;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.fragment.app.Fragment;
 
+import android.app.Notification;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private CardView expensesCard;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        expensesCard.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(MainActivity.this,expensesActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
+
+
 }
